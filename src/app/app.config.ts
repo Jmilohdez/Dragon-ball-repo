c070c6d11d1a2fc9ae600bbc22094027c7717961
Juan Hernandez <camilohdez1017@gmail.com>
2025-08-27 11:01:05 -0500

    Despliegues y HashRouter

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    //HashStrategy estrategia para estar en el root de la single pages, lo que hace es poner un # y hace que no redirecciones a esa carpeta
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    }
   ]
 };
